b78cb1e3d1ee50cae5ae903e82f6e8e949d395ab
Степан <www.niharaktera@yandex.ru>
2023-04-15 20:02:22 +0300

    создал тесты

--- src/test/java/pro/sky/block4/UserTest.java
+++ src/test/java/pro/sky/block4/UserTest.java
@@ -4,4 +4,27 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class UserTest {
+    private final User out = new User("login", "password");
+
+    @Test
+    public void ShouldReturnDefaultMessageIfDataDoNotSetAfterObjectCreating () {
+        boolean result = out.login.equals("login")&&out.password.equals("password");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void ShouldReturnSuccessMessageIfObjectIsCreated () {
+        Assertions.assertNotNull(out);
+    }
+
+    @Test
+    public void ShouldReturnSuccessMessageIfLoginContainsPointandAt () {
+        boolean result = out.login.contains("@")&&out.login.contains(".");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void ShouldReturnSuccessMessageIfLoginAndPasswordAreNotEquals () {
+        Assertions.assertNotEquals(out.login, out.password);
+    }
 }
