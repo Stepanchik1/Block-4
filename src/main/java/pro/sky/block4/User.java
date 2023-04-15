aec088aaf0d33ac3f6e917b898cb20aae0a58523
Степан <www.niharaktera@yandex.ru>
2023-04-15 20:01:09 +0300

    создал юзертест

--- src/main/java/pro/sky/block4/User.java
+++ src/main/java/pro/sky/block4/User.java
@@ -3,4 +3,13 @@ package pro.sky.block4;
 public class User {
     String login;
     String password;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public User() {
+
+    }
 }
---
+++ src/test/java/pro/sky/block4/UserTest.java
@@ -0,0 +1,7 @@
+package pro.sky.block4;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+}
