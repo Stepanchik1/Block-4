6c9ed552706ced47ade6a30886020f54a1469923
Степан <www.niharaktera@yandex.ru>
2023-04-17 16:42:44 +0300

    исправил по подсказкам 2. Прогнал все тесты

--- src/test/java/pro/sky/block4/UserTest.java
+++ src/test/java/pro/sky/block4/UserTest.java
@@ -6,41 +6,55 @@ import org.junit.jupiter.api.Test;
 import java.util.FormatterClosedException;
 
 public class UserTest {
-    private final User out = new User("login", "password");
+    private final User out = new User("@login.ru", "password");
 
     private final User out2 = new User();
 
     @Test
-    public void ShouldReturnDefaultMessageIfLoginDoNotSetAfterObjectCreating () {
-        Assertions.assertEquals("login", out.login);
+    public void ShouldReturnDefaultMessageIfLoginDoNotSetAfterObjectCreating() {
+        Assertions.assertEquals("@login.ru", out.login);
     }
 
     @Test
-    public void ShouldReturnDefaultMessageIfPasswordDoNotSetAfterObjectCreating () {
+    public void ShouldReturnDefaultMessageIfPasswordDoNotSetAfterObjectCreating() {
         Assertions.assertEquals("password", out.password);
     }
 
     @Test
-    public void ShouldReturnSuccessMessageIfLoginIsNotNull () {
-        Assertions.assertNotNull(out2.login);
+    public void ShouldReturnSuccessMessageIfEmptyObjectIsCreated() {
+        Assertions.assertNotNull(out2);
     }
 
     @Test
-    public void ShouldReturnSuccessMessageIfPasswordIsNotNull () {
-        Assertions.assertNotNull(out2.password);
+    public void ShouldReturnSuccessMessageIfPasswordInEmptyObjectIsNull() {
+        Assertions.assertNull(out2.password);
     }
 
-  //   Assertions.assertThrows (FormatterClosedException.class, (User user = new User("NotValidLogin", "password"));} // не знаю как консруктор перевести в лямбду
     @Test
-    public void ShouldReturnSuccessMessageIfLoginContainsPointandAt () {
+    public void ShouldReturnSuccessMessageIfLoginInEmptyObjectIsNull() {
+        Assertions.assertNull(out2.login);
+    }
+
+    @Test
+    public void ShouldReturnDefaultMessageIfObjectWithNonValidPasswordIsCreated() {
+        Assertions.assertThrows(FormatterClosedException.class, (() -> new User("notValid", "password")));
+    }
+
+    @Test
+    public void ShouldReturnDefaultMessageIfObjectWithNonValidLoginIsCreated() {
+        Assertions.assertThrows(FormatterClosedException.class, (() -> new User("@password.ru", "@password.ru")));
+    }
+
+    @Test
+    public void ShouldReturnSuccessMessageIfLoginContainsPointandAt() {
         String notValidLogin = "login";
-        Assertions.assertThrows (FormatterClosedException.class, (() -> out.checkLogin(notValidLogin)));
+        Assertions.assertThrows(FormatterClosedException.class, (() -> out.checkLogin(notValidLogin)));
     }
 
     @Test
-    public void ShouldReturnSuccessMessageIfLoginAndPasswordAreNotEquals () {
+    public void ShouldReturnSuccessMessageIfLoginAndPasswordAreNotEquals() {
         String login = "1@1.1";
         String password = "1@1.1";
-        Assertions.assertThrows (FormatterClosedException.class, (() -> out.checkPasswordAndLogin(login, password)));
+        Assertions.assertThrows(FormatterClosedException.class, (() -> out.checkPasswordAndLogin(login, password)));
     }
 }
