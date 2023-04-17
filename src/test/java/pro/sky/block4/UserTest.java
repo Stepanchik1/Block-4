e3d8d1b50ea3361488ce8cdc8bd5bebf186efdeb
Степан <www.niharaktera@yandex.ru>
2023-04-17 11:37:57 +0300

    исправил по подсказкам

--- src/test/java/pro/sky/block4/UserTest.java
+++ src/test/java/pro/sky/block4/UserTest.java
@@ -3,28 +3,44 @@ package pro.sky.block4;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.FormatterClosedException;
+
 public class UserTest {
     private final User out = new User("login", "password");
 
+    private final User out2 = new User();
+
     @Test
-    public void ShouldReturnDefaultMessageIfDataDoNotSetAfterObjectCreating () {
-        boolean result = out.login.equals("login")&&out.password.equals("password");
-        Assertions.assertTrue(result);
+    public void ShouldReturnDefaultMessageIfLoginDoNotSetAfterObjectCreating () {
+        Assertions.assertEquals("login", out.login);
     }
 
     @Test
-    public void ShouldReturnSuccessMessageIfObjectIsCreated () {
-        Assertions.assertNotNull(out);
+    public void ShouldReturnDefaultMessageIfPasswordDoNotSetAfterObjectCreating () {
+        Assertions.assertEquals("password", out.password);
     }
 
+    @Test
+    public void ShouldReturnSuccessMessageIfLoginIsNotNull () {
+        Assertions.assertNotNull(out2.login);
+    }
+
+    @Test
+    public void ShouldReturnSuccessMessageIfPasswordIsNotNull () {
+        Assertions.assertNotNull(out2.password);
+    }
+
+  //   Assertions.assertThrows (FormatterClosedException.class, (User user = new User("NotValidLogin", "password"));} // не знаю как консруктор перевести в лямбду
     @Test
     public void ShouldReturnSuccessMessageIfLoginContainsPointandAt () {
-        boolean result = out.login.contains("@")&&out.login.contains(".");
-        Assertions.assertTrue(result);
+        String notValidLogin = "login";
+        Assertions.assertThrows (FormatterClosedException.class, (() -> out.checkLogin(notValidLogin)));
     }
 
     @Test
     public void ShouldReturnSuccessMessageIfLoginAndPasswordAreNotEquals () {
-        Assertions.assertNotEquals(out.login, out.password);
+        String login = "1@1.1";
+        String password = "1@1.1";
+        Assertions.assertThrows (FormatterClosedException.class, (() -> out.checkPasswordAndLogin(login, password)));
     }
 }
